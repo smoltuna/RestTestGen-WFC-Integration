328690adb8aee2b5fde4e4e562c0505c6b6516e3
EdoBaz <edobaz02@gmail.com>
2025-12-11 16:02:41 +0100

    Update flowable-process

--- RESTgym/apis/flowable-process/auth.py
+++ RESTgym/apis/flowable-process/auth.py
@@ -1,16 +1,13 @@
-"""
-Flowable Process Engine - Authentication Script
-Basic Auth with configured credentials
-"""
 import base64
 
 class FlowableAuth:
     def __init__(self):
         self.username = "restapitestteam@gmail.com"
-        self.password = "universe"
+        self.password = "restapitestteam"
         self.auth_header = None
     
     def get_auth_header(self):
+        """Generate Basic Auth header with configured credentials"""
         if self.auth_header is None:
             credentials = f"{self.username}:{self.password}"
             encoded = base64.b64encode(credentials.encode()).decode()
@@ -18,7 +15,7 @@ class FlowableAuth:
         return self.auth_header
     
     def request(self, flow):
-        # Add Basic Auth header to all requests
+        """Add Basic Auth header to all requests"""
         flow.request.headers["Authorization"] = self.get_auth_header()
 
 addons = [FlowableAuth()]
